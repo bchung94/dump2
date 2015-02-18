25f832464545573a4af75d84df9b705b89b4af62
Emily <sweet4lorie@gmail.com>
2015-02-18 10:08:57 -0800

    Deleted: - removed Standard Asset since we were not using it
    Edited:
    - moved scripts outside folders in the 'script' folder in 'import assets'
    - reconnected moved scripts
    
    Added:
    - transitions added in Animation Controller between run and jump animations on H-Robot and C-Robot
    - added transitions into p1 and p2move scripts for jumpin and landing
    
    Errors:
    - Animation rotations for H-Robot jump and idle wrong, need fix and re-exporting

--- GAME/Assets/ImportedAssets/Scripts/P1move.cs
+++ GAME/Assets/ImportedAssets/Scripts/P1move.cs
@@ -3,20 +3,25 @@ using System.Collections;
 
 public class P1move : MonoBehaviour {
 
+	private Animator animator;
+
 	private Vector3 forward = new Vector3(0, 0, 5);
 	private Vector3 side = new Vector3(5, 0, 0);
 	private Vector3 up = new Vector3(0, 6, 0);
 	public bool isgrounded;
-	// Use this for initialization
+
+	// Use this for initialization	
 	void Start () {
-	
+		animator = GetComponent<Animator>();
+		animator.SetBool("Jump", false);
 	}
 
 	void OnCollisionEnter (Collision collision) {
-			if (collision.gameObject.tag == "Floor") {
-					isgrounded = true;
-				}
+		if (collision.gameObject.tag == "Floor") {
+			isgrounded = true;
+			animator.SetBool("Jump", false);
 		}
+	}
 
 	void OnCollisionExit (Collision collision) {
 		if (collision.gameObject.tag == "Floor") {
@@ -35,6 +40,7 @@ public class P1move : MonoBehaviour {
 		if ((Input.GetKey (KeyCode.Space))&&(isgrounded == true)) {
 			//rigidbody.AddForce (0, speed*2, 0);
 			rigidbody.velocity = up;
+			animator.SetBool("Jump", isgrounded);
 		}
 		rigidbody.transform.rotation = Quaternion.identity;
 		rigidbody.transform.Rotate (0, 270, 0);
--- GAME/Assets/ImportedAssets/Scripts/P2move.cs
+++ GAME/Assets/ImportedAssets/Scripts/P2move.cs
@@ -3,18 +3,23 @@ using System.Collections;
 
 public class P2move : MonoBehaviour {
 
+	private Animator animator;
+
 	private Vector3 forward = new Vector3(0, 0, 5);
 	private Vector3 side = new Vector3(5, 0, 0);
 	private Vector3 up = new Vector3(0, 6, 0);
 	public bool isgrounded;
+
 	// Use this for initialization
 	void Start () {
-		
+		animator = GetComponent<Animator>();
+		animator.SetBool("Jump", false);
 	}
 	
 	void OnCollisionEnter (Collision collision) {
 		if (collision.gameObject.tag == "Floor") {
-			isgrounded = true;
+			isgrounded = true;	
+			animator.SetBool("Jump", false);
 		}
 	}
 	
@@ -32,9 +37,10 @@ public class P2move : MonoBehaviour {
 		if (Input.GetKey (KeyCode.RightArrow)) {
 			rigidbody.MovePosition(rigidbody.position + side * Time.deltaTime);
 		}
-		if ((Input.GetKey (KeyCode.RightShift))&&(isgrounded == true)) {
+		if ((Input.GetKey (KeyCode.UpArrow)) && (isgrounded == true)) {
 			//rigidbody.AddForce (0, speed*2, 0);
 			rigidbody.velocity = up;
+			animator.SetTrigger("Jump");
 		}
 		rigidbody.transform.rotation = Quaternion.identity;
 	}
