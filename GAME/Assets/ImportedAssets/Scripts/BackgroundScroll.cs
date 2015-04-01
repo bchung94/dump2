576b6109b37943cf00b8dbd19a4768c9cb5e62e7
Emily <sweet4lorie@gmail.com>
2015-03-31 23:28:18 -0700

    fixing previous add issues.

--- GAME/Assets/ImportedAssets/Scripts/BackgroundScroll.cs
+++ GAME/Assets/ImportedAssets/Scripts/BackgroundScroll.cs
@@ -2,9 +2,13 @@
 using System.Collections;
 
 public class BackgroundScroll : MonoBehaviour {
-	public float speed = 0.5f;
+
+	public float normalSpeed = 0.1f;
+	public float fastSpeed = 0.5f;
+	public float speed;
 	// Use this for initialization
 	void Start () {
+		speed = normalSpeed;
 	}
 
 	// Update is called once per frame
